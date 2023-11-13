3a087ae64005472bc078778b53040d03b874f820
Someone Serge <sergei.kozlukov@aalto.fi>
2023-11-13 21:59:18 +0000

    where-so: only print out the ORIGIN

--- where-so.c
+++ where-so.c
@@ -29,40 +29,6 @@ int main(int argc, const char **argv) {
       continue;
     }
     printf("%s\n", path);
-
-    Dl_serinfo peek;
-    if (dlinfo(handle, RTLD_DI_SERINFOSIZE, &peek) == -1) {
-      status = EXIT_FAILURE;
-      fprintf(stderr, "RTLD_DI_SERINFOSIZE failed for %s. Reason: %s\n",
-              argv[i], dlerror());
-      continue;
-    }
-
-    {
-      Dl_serinfo *info = (Dl_serinfo *)malloc(peek.dls_size);
-      if (!info) {
-        perror("malloc");
-        return EXIT_FAILURE;
-      }
-
-      if (dlinfo(handle, RTLD_DI_SERINFOSIZE, info) == -1) {
-        free(info);
-        status = EXIT_FAILURE;
-        fprintf(stderr, "RTLD_DI_SERINFOSIZE (2nd) failed: %s\n", dlerror());
-        continue;
-      }
-      if (dlinfo(handle, RTLD_DI_SERINFO, info) == -1) {
-        free(info);
-        status = EXIT_FAILURE;
-        fprintf(stderr, "RTLD_DI_SERINFO failed for %s. Reason: %s\n", argv[i],
-                dlerror());
-        continue;
-      }
-      for (int j = 0; j < info->dls_cnt; ++j) {
-        printf("%s\n", info->dls_serpath[j].dls_name);
-      }
-      free(info);
-    }
   }
   return status;
 }
